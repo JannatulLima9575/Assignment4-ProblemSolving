30b368e147c3f77352b0546d45826baf74b28c79
Jannatul <limajannat1675@gmail.com>
2025-02-17 22:47:05 +0600

    added problem-2.js

--- problem.js
+++ problem.js
@@ -11,3 +11,26 @@ function cashOut( money ) {
 let result = cashOut(2000);
 console.log(result);
 
+// # Problem-02 : Detect email  valid or not
+function  validEmail( email ) {
+    if (typeof email !== "string") {
+        return "Invalid";
+    }
+    
+    if (/^[.\-+_@]/.test(email)) {
+        return false;
+    }
+    else if (!email.includes("@")) {
+        return false;
+    }
+    else if (email.includes(" ")) {
+        return false;
+    }
+    else if (!email.endsWith(".com")) {
+        return false;
+    }
+    return true;
+}
+
+let emailResult = validEmail("lima@gmail.com");
+console.log(emailResult);
